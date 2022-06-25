5878b028a4c2f6b5a66cfce0aa316d59da75dde2
Илья Горшенев <gorschenyov.ilya2014@yandex.ru>
2022-06-25 22:32:03 +0300

    Исправления по второму модулю

--- js/main.js
+++ js/main.js
@@ -2,30 +2,19 @@
 
 function random(min, max) {
   if (max < min) {                   //Если значение "до" меньше, значит случайно число не может быть положительным, что не соответствует условиям
-    return false;
-  };
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    throw new Error('Некорректные диапазон');
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-
-
-console.log('Number ' + random(0, 100)); //Результат: случайное целое число из диапазона от 0 до 100
+random(0, 100); //Результат: случайное целое число из диапазона от 0 до 100
 
 
 // https://habr.com/ru/company/ruvds/blog/534108/  - ссылка на функцию
 
 // Функция для проверки максимальной длины строки
 
-function getLength (inputText, maxLength) {
-  let field = inputText.length   // Измеряет длину введенной строки и сравнивает
-
-  if (field <= maxLength) {
-    return true;
-  }
-  return false;
-};
-
-console.log(getLength ('Проверка длины строки', 1000));
-
+const checkLength = (inputText, maxLength) => inputText.length <= maxLength;
+checkLength ('Проверка длины строки', 100);
 
 //http://kodesource.top/javascript/form/string-length.php - ссылка на функцию для проверки длины строки
