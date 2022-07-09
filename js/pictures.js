cf0355c09489e69c871a1a304a7151f846656007
Илья Горшенев <gorschenyov.ilya2014@yandex.ru>
2022-07-09 12:36:54 +0300

    Исправления линта

--- js/pictures.js
+++ js/pictures.js
@@ -2,20 +2,17 @@ import {allObjects} from './data.js';
 
 const pictures = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture')
-.content
-.querySelector('.picture');
-
-
+  .content
+  .querySelector('.picture');
 
 const objectFragment = document.createDocumentFragment();
 
 allObjects.forEach((object) => {
   const newPicture = pictureTemplate.cloneNode(true);
-  console.log(newPicture);
   newPicture.querySelector('.picture__img').src = object.url;
   newPicture.querySelector('.picture__likes').textContent = object.likes;
   newPicture.querySelector('.picture__comments').textContent = object.comment.length;
   objectFragment.appendChild(newPicture);
-})
+});
 
 pictures.appendChild(objectFragment);
