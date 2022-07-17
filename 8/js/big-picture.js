c5d814fd477c0a64ba8a4f82c8c1fb58f292c7df
Keks <keks@htmlacademy.ru>
2022-07-17 16:29:19 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/big-picture.js
@@ -0,0 +1,90 @@
+import {allObjects} from './data.js';
+
+const isEscapeKey = function (evt) {
+  return evt.key === 'Escape';
+};
+
+const big = document.querySelector('.big-picture');
+const bigImage = document.querySelector('.big-picture__img').querySelector('img');
+const likes = document.querySelector('.likes-count');
+const bigComments = document.querySelector('.comments-count');
+const allComments = document.querySelector('.social__comments');
+const commentsLoader = document.querySelector('.comments-loader');
+const pictures = document.querySelectorAll('.picture');
+
+const makeElement = function (tagName, className, text) {
+  const element = document.createElement(tagName);
+  element.classList.add(className);
+  if (text) {
+    element.textContent = text;
+  }
+  return element;
+};
+
+const descriptionPlace = document.querySelector('.social__caption');
+const commentsBlocks = document.querySelector('.social__comment-count');
+
+const pushComment = function (comment) {
+  const commentsItem = makeElement('li', 'social__comment');
+  const commentsImage = makeElement('img', 'social__picture');
+  commentsImage.src = comment.avatar;
+  commentsImage.alt = comment.Name;
+  commentsItem.appendChild(commentsImage);
+  const commentsText = makeElement('p', 'social__text', comment.message);
+  commentsItem.appendChild(commentsText);
+  return commentsItem;
+};
+
+const openBigPicture = function (picture, comments, description) {
+  const picturePhoto = picture.querySelector('img');
+  const pictureLikes = picture.querySelector('.picture__likes');
+  const pictureComments = picture.querySelector('.picture__comments');
+
+  picture.addEventListener('click',() => {
+    allComments.innerHTML = '';
+    bigImage.src = picturePhoto.src;
+    likes.textContent = pictureLikes.textContent;
+    bigComments.textContent = pictureComments.textContent;
+    descriptionPlace.textContent = description;
+
+    for (let i = 0; i < comments.length; i++) {
+      const cardItem = pushComment(comments[i]);
+      allComments.appendChild(cardItem);
+    }
+
+    openPicture();
+  });
+};
+
+for (let i = 0; i < pictures.length; i++) {
+  openBigPicture(pictures[i], allObjects[i].comment, allObjects[i].description);
+}
+
+const onPictureEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closePicture();
+  }
+};
+
+function openPicture () {
+  commentsLoader.classList.add('hidden');
+  commentsBlocks.classList.add('hidden');
+  big.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  document.addEventListener('keydown', onPictureEscKeydown);
+}
+
+const bigPictureClose = document.querySelector('.big-picture__cancel');
+
+function closePicture () {
+  big.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onPictureEscKeydown);
+}
+
+bigPictureClose.addEventListener('click',() => {
+  closePicture();
+});
+
+export {isEscapeKey};
---
+++ 8/js/forms.js
@@ -0,0 +1,181 @@
+const uploadInput = document.querySelector('#upload-file');
+const uploadOverlay = document.querySelector('.img-upload__overlay');
+const uploadCancel = document.querySelector('#upload-cancel');
+const description = document.querySelector('.text__description');
+const hashtag = document.querySelector('.text__hashtags');
+const form = document.querySelector('#upload-select-image');
+const objectFragment = document.createDocumentFragment();
+
+const closeOverlay = function () {
+  uploadOverlay.classList.add('hidden');
+  document.body.classList.remove('.modal-open');
+  hashtag.value = '';
+  uploadInput.value = '';
+  description.value = '';
+};
+
+const onOverlayEscKeydown = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closeOverlay();
+  }
+};
+
+const openOverlay = function () {
+  uploadOverlay.classList.remove('hidden');
+  document.body.classList.add('.modal-open');
+  document.addEventListener('keydown', onOverlayEscKeydown);
+};
+
+uploadInput.addEventListener('change', openOverlay);
+
+uploadCancel.addEventListener('click', () => {
+  closeOverlay();
+  document.removeEventListener('keydown', onOverlayEscKeydown);
+});
+
+const errorModal = function() {
+  const error = document.querySelector('.error');
+  if (error) {
+    document.body.removeChild(error);
+  }
+  document.addEventListener('keydown', onOverlayEscKeydown);
+};
+const successModal = function() {
+  const success = document.querySelector('.success');
+  if (success) {
+    document.body.removeChild(success);
+  }
+  document.addEventListener('keydown', onOverlayEscKeydown);
+};
+
+const onErrorModalEscKeydown = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    errorModal();
+  }
+};
+
+const onSuccessModalEscKeydown = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    successModal();
+  }
+};
+
+const showSuccess = function() {
+  const successTemplate = document.querySelector('#success').content.querySelector('.success');
+  const successClone = successTemplate.cloneNode(true);
+  objectFragment.appendChild(successClone);
+  document.body.appendChild(objectFragment);
+  document.removeEventListener('keydown', onOverlayEscKeydown);
+  document.addEventListener('keydown', onSuccessModalEscKeydown);
+  document.querySelector('.success').addEventListener('click', () => {successModal();
+    document.removeEventListener('keydown', onSuccessModalEscKeydown);
+  });
+  document.querySelector('.success__inner').addEventListener('click', (evt) => {
+    evt.stopPropagation();
+  });
+  document.querySelector('.success__button').addEventListener('click', (evt) => {
+    if (evt.target.tagName === 'BUTTON') {
+      successModal();
+      document.removeEventListener('keydown', onSuccessModalEscKeydown);
+    }
+  });
+};
+
+const showError = function() {
+  const errorTemplate = document.querySelector('#error').content.querySelector('.error');
+  const errorClone = errorTemplate.cloneNode(true);
+  objectFragment.appendChild(errorClone);
+  document.body.appendChild(objectFragment);
+  document.removeEventListener('keydown', onOverlayEscKeydown);
+  document.addEventListener('keydown', onErrorModalEscKeydown);
+  document.querySelector('.error').addEventListener('click', () => {errorModal();
+    document.removeEventListener('keydown', onErrorModalEscKeydown);
+  });
+  document.querySelector('.error__inner').addEventListener('click', (evt) => {
+    evt.stopPropagation();
+  });
+  document.querySelector('.error__button').addEventListener('click', (evt) => {
+    if (evt.target.tagName === 'BUTTON') {
+      errorModal();
+      document.removeEventListener('keydown', onErrorModalEscKeydown);
+    }
+  });
+};
+hashtag.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    evt.stopPropagation();
+  }
+});
+
+description.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    evt.stopPropagation();
+  }
+});
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'text__item--invalid',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'span',
+  errorTextClass: 'text__error'
+});
+
+
+const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+
+const checkHashtags = function (value) {
+  if (value === '') {
+    return true;
+  }
+  const array = value.split(' ');
+  const newArray = [];
+  if (array.length > 5) {
+    return false;
+  }
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(re.test(array[i]));
+  }
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    array.splice(i, 1);
+    if (array.includes(element)) {
+      return false;
+    }
+  }
+  if (newArray.includes(false)) {
+    return false;
+  } else {return true;}
+};
+
+const checkDescription = function (value) {
+  if (value.length > 140) {
+    return false;
+  }
+  return true;
+};
+
+pristine.addValidator(
+  form.querySelector('#hashtags'),
+  checkHashtags,
+  'Ошибка при вводе значений'
+);
+
+pristine.addValidator(
+  description,
+  checkDescription,
+  'Максимальная длина 140 символов'
+);
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const isValid = pristine.validate();
+  if (isValid) {
+    showSuccess();
+  } else {
+    showError();
+  }
+});
